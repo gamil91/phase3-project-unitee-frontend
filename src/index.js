58114f863b72f610c0ad7e7355f42cf576261612
gamil91 <alexis_gamil99@yahoo.com.com>
2020-12-02 13:55:16 -0800

    finished working on the frontend of viewing a shirt

--- src/index.js
+++ src/index.js
@@ -1,32 +1,39 @@
+const navShirts = document.querySelector("#shirts")
+navShirts.addEventListener("click", getItems)
+
 const div = document.querySelector("#div-container")
 div.addEventListener("click", handleDivEvents)
 
-const divShow = document.querySelector("#div-show")
 const divItemContainer = document.querySelector("#div-item-container")
-
+//div holding the shirt in purchase page
 const divItem = document.createElement("div")
 divItem.id = "div-item"
-
+//div holding the shirt's info and purchase form in purchase page
 const divInfo = document.createElement("div")
 divInfo.id = "div-info"
 
-const divSelect = document.createElement("div")
-divSelect.id = "div-select"
+const divForm = document.createElement("div")
+divForm.id = "div-select"
+
 
 
 
 getItems()
+createCart()
 
 function getItems(){
+    div.innerHTML = ""
+    divItemContainer.innerHTML = ""
     fetch("http://localhost:3000/items")
     .then(res => res.json())
     .then(items => items.forEach(item => showEachItem(item)))
 }
 
 function showEachItem(item){
+
+    const divItemHome = document.createElement("div")
+    divItemHome.className = "col-4"
     
-    const divItem = document.createElement("div")
-    divItem.className = "div-item"
 
     const image = document.createElement("img")
     image.src = item.images[1].image_url
@@ -37,27 +44,28 @@ function showEachItem(item){
     p.textContent = item.name
     p.id = item.id
 
-    divItem.append(image, p)
-    divItemContainer.appendChild(divItem)
+    divItemHome.append(image, p)
+    divItemContainer.appendChild(divItemHome)
     div.appendChild(divItemContainer)
 }
 
 
 function handleDivEvents(e){
     if (e.target.className == "item-image" || e.target.matches('p')){ 
-        // div.textContent = ""
         divItemContainer.textContent = ""
         fetch("http://localhost:3000/items/" + e.target.id)
         .then(res => res.json())
         .then(item => showItem(item))
     }
-    console.log(e.target)
+    // console.log(e.target)
 
 }
 
 function showItem(item){
+    //divItem -> div holding the shirt image
     divItem.textContent = ""
     divInfo.textContent = ""
+    divForm.textContent = ""
 
     const image = document.createElement("img")
     image.src = item.images[1].image_url
@@ -65,31 +73,101 @@ function showItem(item){
     const name = document.createElement("p")
     name.id = item.id
     name.textContent = item.name
+
     const price = document.createElement("p")
     price.id = item.id
     price.textContent = `$ ${item.price}`
 
-    
-    const select = document.createElement("select");
-    select.addEventListener("change", (e) =>{
-        image.src = e.target.value
-    } )
-    
-    item.images.forEach(image => {
-        const color = image.color
-        const option = document.createElement("option");
-        option.value = image.image_url
-        option.text = color.charAt(0).toUpperCase() + color.slice(1);
-        select.appendChild(option)
-    })
+    const ulColor = document.createElement("ul")
+    ulColor.id = "horizontal-list"
+    ulColor.innerHTML = `View Available Colors: <br/><br/>`
 
-    const label = document.createElement("label");
-    label.innerHTML = "Choose a color "
+    // divForm.append(label, selectColor)
+    divForm.innerHTML = `
+    <form>
+    <div class="form-group">
+        <select class="form-control custom-select" id="selectQuantity" name="quantity">
+        <option>Quantity</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        </select>
+    
+    
+        <label for="exampleFormControlSelect1">Choose a color</label>
+        <select class="form-control custom-select" id="selectColor" name="color">
+        </select>
+   
+    
+        <label for="exampleFormControlSelect2">Choose a size</label>
+        <select multiple class="form-control custom-select" id="exampleFormControlSelect2" name="size">
+        <option value="Extra Small">Extra Small</option>  
+        <option value="Small">Small</option>
+        <option value="Medium">Medium</option>
+        <option value="Large">Large</option>
+        <option value="Extra Large">Extra Large</option>
+        </select>
+    </div>
+    <button type="submit" class="btn btn-primary mb-2">Add to cart</button>
+    </form>
+    `
 
-    divSelect.append(label, select)
     divItem.appendChild(image)
-    divInfo.append(name, price, divSelect)
+    divInfo.append(name, price, ulColor, divForm)
 
-    divShow.append(divItem, divInfo)
-    div.appendChild(divShow)
+    //appending two divs into innerdiv
+    divItemContainer.append(divItem, divInfo)
+    //appending innderdiv to bigDiv
+    div.appendChild(divItemContainer)
+
+    //grabbing select from the DOM populating it with each item's image
+    const selectColor = document.querySelector("#selectColor");
+    
+    ulColor.addEventListener("click", (e) =>{
+        if (e.target.matches("button")){
+        image.src = e.target.id
+        }
+        
+    })
+        item.images.forEach(image => {
+            //creating options for the color selector
+            const color = image.color
+            const option = document.createElement("option");
+            option.value = color
+            option.text = color.charAt(0).toUpperCase() + color.slice(1);
+            //creating buttons for colors for preview
+            const liColor = document.createElement("li")
+            const btnColor = document.createElement("button")
+            btnColor.className = "btn btn-outline-dark btn-sm"
+            btnColor.textContent = color
+            btnColor.id = image.image_url
+            liColor.appendChild(btnColor)
+            
+
+            ulColor.appendChild(liColor)
+            selectColor.appendChild(option)
+        })
+
+    const form = document.querySelector("form")
+    form.addEventListener("submit", (e) => addToCart(e, item) )
+
 }
+
+function addToCart(e, item){
+    e.preventDefault()
+
+    formData = {
+        quantity: e.target.quantity.value,
+        color: e.target.color.value,
+        size: e.target.size.value,
+        item_id: item.id,
+        cart_id: something 
+    }
+
+
+
+
+}
+
